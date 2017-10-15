759e9765c12d2c7618022edd4fed9c05d1df051c
boorish92@gmail.com <boorish92@gmail.com>
2017-10-15 23:57:15 +0200

    blog init

--- src/js/Containers/Blog.js
+++ src/js/Containers/Blog.js
@@ -6,7 +6,38 @@ class Blog extends Component {
   render() {
     return (
       <div className="blog">
+        <div className="blog-post">
+          <div className='blog-post__cover'>
 
+          </div>
+          <div className="blog-post__contentBox">
+            <div className="blog-post__mapBox">
+
+            </div>
+            <div className="blog-post__content">
+              <h2 className="blog-post__title"> Week in Alps </h2>
+              <div className="blog-post__date"> created at: <span> 15.10.2017 </span> </div>
+              <hr />
+              <div className="blog-post__tags">
+                <ul>
+                  <li> hiking </li>
+                  <li> train </li>
+                  <li> duo </li>
+                  <li> high-moutains </li>
+                  <li> autumn </li>
+                </ul>
+              </div>
+              <div className="blog-post__timeLine">
+                <div className="blog-post__stage">
+                  <div className="blog-post__stage-date"> 10.10 </div>
+                  <div className="blog-post__stage-place"> Neuschwanstein </div>
+                </div>
+              </div>
+
+              <div className='blog-post__page-number'> 1/4 </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
